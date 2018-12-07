bd9da916e62e13325ea7e855a693df012e64b488
Clayton Wang <claydwang@gmail.com>
2018-12-07 00:33:21 -0800

    responsive

--- js/index.js
+++ js/index.js
@@ -20,3 +20,39 @@ function activeHandler(array) {
 }
 
 activeHandler(sideNavItem);
+
+var aboutLink = document.querySelector('.link-1');
+var workLink = document.querySelector('.link-2');
+var aboutSect = document.querySelector('.about-work__about');
+var workSect = document.querySelector('.about-work__work');
+
+aboutLink.addEventListener('click', function() {
+  console.log(aboutSect.classList);
+  aboutSect.classList.remove('about-work__about--active');
+  workSect.classList.add('about-work__work--active');
+  setTimeout(moveTwo, 1500);
+});
+
+workLink.addEventListener('click', function() {
+  aboutSect.classList.add('about-work__about--active');
+  workSect.classList.remove('about-work__work--active');
+  setTimeout(moveOne, 1500);
+});
+
+function moveOne() {
+  setTimeout(function() {
+    console.log('last');
+    workSect.style.display = 'block';
+    aboutSect.style.display = 'none';
+    elem.style.width = 0 + '%';
+  }, 1500);
+}
+
+function moveTwo() {
+  setTimeout(function() {
+    console.log('last');
+    workSect.style.display = 'none';
+    aboutSect.style.display = 'block';
+    elem.style.width = 0 + '%';
+  }, 1500);
+}
